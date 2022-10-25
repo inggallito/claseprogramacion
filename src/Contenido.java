aa90c0e29db0c71494b2f8dabe82234360222819
inggallito <116390297+inggallito@users.noreply.github.com>
2022-10-25 14:31:06 -0500

    Contenido

---
+++ src/Contenido.java
@@ -0,0 +1,25 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("Lenguajes de preogramacion");
+        System.out.println("Entornos de pregoramacion");
+        System.out.println("Importancia de la programacion");
+        System.out.println("Creacion de Algoritmos");
+        System.out.println("Lenguaje de programacion java");
+        System.out.println("Metodologias de programacion");
+        System.out.println("Software de sistema");
+        System.out.println("Software de aplicacion");
+        System.out.println("Formas de escritura de algoritmos");
+        System.out.println("Software");
+        System.out.println("Algortimo PSeudocódigo");
+        System.out.println("Diagramas de flujo");
+        System.out.println("Partes del Algoritmo");
+        System.out.println("Entradas del Algoritmo");
+        System.out.println("Procesos del Algoritmo");
+        System.out.println("Salida del Algoritmo");
+        System.out.println("Analisis del Algoritmo");
+        System.out.println("Diseños del Algoritmo");
+        System.out.println("Pruebas del Algoritmo");
+
+    }
+
+}
---
+++ src/Git.java
@@ -0,0 +1,14 @@
+public class Git {
+    public static void main(String[] args) {
+            System.out.println("Se inicia secion en github en la terminal");
+            System.out.println("Creamos un repositorio en github, donde posteriormente se subira el archivo");
+            System.out.println("Para iniciar un archivo git, primeramente se coloca el comando Git init");
+            System.out.println("Posteriormente de usa el comando Git Add para mandar el codigo a preparacion");
+            System.out.println("Se puede usar git status para revisar el estado del codigo" );
+            System.out.println("Colocamos git brancj -M main");
+            System.out.println("Despues se coloca git remote add origin para colocar el origen del repositorio y poder subir el archivo a github");
+            System.out.println( "Y colocamos push -u origin main para subir el archivo a github en el repositorio remoto");
+
+
+    }
+
